29191cd5c63a88e81a53952b185777b31112286f
fanyi <yngwiefy82@gmail.com>
2018-08-27 14:59:03 +0800

    spine查找图集规则 1、查找与json同名的图集（老规则） 2、查找与所在文件夹同名的图集

--- extensions/spine/editor/spine-meta.js
+++ extensions/spine/editor/spine-meta.js
@@ -9,7 +9,7 @@ const Fs = require('fire-fs');
 const Path = require('fire-path');
 const Spine = require('../lib/spine');
 
-const ATLAS_EXTS = ['.atlas', '.txt', '.atlas.txt', ''];
+const ATLAS_EXTS = ['.atlas',''];
 const SPINE_ENCODING = { encoding: 'utf-8' };
 
 const CustomAssetMeta = Editor.metas['custom-asset'];
@@ -17,9 +17,21 @@ const CustomAssetMeta = Editor.metas['custom-asset'];
 function searchAtlas (skeletonPath, callback) {
     skeletonPath = Path.stripExt(skeletonPath);
 
+
     function next (index) {
-        var suffix = ATLAS_EXTS[index];
-        var path = skeletonPath + suffix;
+        var path = '';
+
+        if (index < 1)
+        {
+            var suffix = ATLAS_EXTS[index];
+            path = skeletonPath + suffix;
+        }
+        else
+        {
+            path = skeletonPath.substr(0, skeletonPath.lastIndexOf('/'));
+            path = path + "/" + path.substr(path.lastIndexOf('/') + 1, path.length - path.lastIndexOf('/')) +".atlas";
+        }
+
         Fs.exists(path, exists => {
             if (exists) {
                 return callback(null, path);
@@ -28,7 +40,7 @@ function searchAtlas (skeletonPath, callback) {
                 next(index + 1);
             }
             else {
-                callback(new Error(`Can not find ${skeletonPath + ATLAS_EXTS[0]}`));
+                callback(new Error(`Can not find ${skeletonPath + ATLAS_EXTS[0]} | ${atlasPath}`));
             }
         });
     }
@@ -143,7 +155,7 @@ class SpineMeta extends CustomAssetMeta {
         }
         return res;
     }
-    
+
     postImport (fspath, cb) {
         Fs.readFile(fspath, SPINE_ENCODING, (err, data) => {
             if (err) {
@@ -188,21 +200,21 @@ class SpineMeta extends CustomAssetMeta {
 
                 //
                 asset.atlasText = res.data;
-                
+
                 // save raw assets for JSB..
-                
+
                 var atlasUuid = db.fspathToUuid(res.atlasPath);
                 asset.atlasUrl = db.uuidToUrl(atlasUuid);
-                
+
                 db.mkdirForAsset(this.uuid);
                 var rawJsonPath = Path.join(db._uuidToImportPathNoExt(this.uuid), RAW_SKELETON_FILE);
                 Fs.copySync(fspath, rawJsonPath);
                 asset._setRawFiles([RAW_SKELETON_FILE]);
-                
+
                 this.atlas = atlasUuid;     // save for dest()
-                
+
                 //
-                
+
                 db.saveAssetToLibrary(this.uuid, asset);
                 cb();
             });
